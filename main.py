82224fe39c9b733a3f9d7ec7ca052fc5b68fb1d9
Pedro Martos <54295971+P2D3R4@users.noreply.github.com>
2022-05-09 22:55:47 +0000

    Alterado

--- main.py
+++ main.py
@@ -5,9 +5,5 @@ app = Flask('app')
 def index():
   return render_template('index.html')
 
-@app.route('/unifran')
-def unifran():
-  return render_template('unifran.html')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
